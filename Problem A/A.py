4f92f8a2961355171a013c5b8bf8c4ea83d600e3
Pritimoy Sarkar <pritimoysarkar@gmail.com>
2020-06-21 22:56:43 +0530

    Update A.py

--- Problem A/A.py
+++ Problem A/A.py
@@ -3,7 +3,7 @@ for j in range(T):
     n=int(input())
     sum=1
     count=1
-    for i in range(2,n):
+    for i in range(2,n+1):
         if(sum>=n): break
         if(i>sum):
             count+=1
